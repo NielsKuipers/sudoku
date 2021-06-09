a726d0fb57c871b7b5326177f5e9b6056361b146
NielsKuipers <cutebou@hotmail.nl>
2021-06-09 14:06:57 +0200

    implemented filereader

--- sudoku/Builder/SudokuReader.cs
+++ sudoku/Builder/SudokuReader.cs
@@ -1,10 +1,27 @@
-﻿namespace sudoku.Builder
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace sudoku.Builder
 {
     public class SudokuReader
     {
         public void ReadFile()
         {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "txt files (*.txt)|*.txt"
+            };
+
+            if (openFileDialog.ShowDialog() != true) return;
+
+            var path = openFileDialog.FileName;
+            var lines = File.ReadAllLines(path);
             
+            foreach (var line in lines)
+            {
+            }
         }
     }
 }
--- sudoku/MainWindow.xaml.cs
+++ sudoku/MainWindow.xaml.cs
@@ -1,13 +1,25 @@
-﻿namespace sudoku
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+using sudoku.Builder;
+
+namespace sudoku
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private SudokuReader reader;
         public MainWindow()
         {
+            reader = new SudokuReader();
             InitializeComponent();
+        }
+
+        private void readfile_Click(object sender, RoutedEventArgs e)
+        {
+            reader.ReadFile();
         }
     }
 }
