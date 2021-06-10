37dea7751f8bc95f94354b4e92095fabe439ddf1
NielsKuipers <cutebou@hotmail.nl>
2021-06-10 19:16:40 +0200

    displaying sudoku :)

--- sudoku/Views/SudokuUserControl.xaml.cs
+++ sudoku/Views/SudokuUserControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
 using sudoku.SudokuBoard;
 
 namespace sudoku.Views
@@ -10,6 +12,26 @@ namespace sudoku.Views
         {
             InitializeComponent();
             _board = board;
+            var grid = (Grid) FindName("sudokuGrid");
+
+            for (var i = 0; i < _board.Regions.GetCount(); i++)
+            {
+                grid?.ColumnDefinitions.Add(new ColumnDefinition {Width = new GridLength(20)});
+                grid?.RowDefinitions.Add(new RowDefinition{Height = new GridLength(20)});
+                
+                var curRegion = _board.Regions.Get(i);
+
+                for (var j = 0; j < curRegion.GetCount(); j++)
+                {
+                    var temp = curRegion.Get(j);
+                    var box = new TextBox {Text = temp.Value.ToString()};
+
+                    Grid.SetColumn(box, temp.X);
+                    Grid.SetRow(box, temp.Y);
+
+                    grid?.Children.Add(box);
+                }
+            }
         }
     }
 }
